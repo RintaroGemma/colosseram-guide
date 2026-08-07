fd517b263b50353791ccdba57ba16a151fa1036b
RintaroGemma <hagamonote@gmail.com>
2026-08-07 20:49:45 +0200

    feature: 原稿機能追加

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="colosseo-guide-v2";
+const CACHE="colosseo-guide-v3";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
